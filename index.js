abbfa9bd07f3be643165cd27bdd037140fa07f0f
pankaj <pankajkumar70792@gmail.com>
2024-01-11 00:08:07 +0530

    addedd register user

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const { dbConnect } = require("./config/dbconnect");
+const { dbConnect } = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
+const authRoute = require("./routes/authRoute");
+const bodyParser = require("body-parser");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 
 dbConnect();
-
-app.use("/", (req, res) => {
-  res.send("Hello from server side");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/user", authRoute);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server is running at PORT ${PORT}`);
---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = mongoose.model("User", userSchema);
