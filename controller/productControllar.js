f82ab641b156bcb772b7fab97deb56eccc31c4b5
pankaj <pankajkumar70792@gmail.com>
2024-01-20 01:44:26 +0530

    Created rating API in Product

--- controller/productControllar.js
+++ controller/productControllar.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const slugify = require("slugify");
 
@@ -119,6 +120,90 @@ const filterProduct = asyncHandler(async (req, res) => {
   } catch (error) {}
 });
 
+const addToWishlist = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { prodId } = req.body;
+  console.log(prodId);
+  try {
+    const user = await User.findById(_id);
+    const alreadyadded = user.wishlist.find(
+      (id) => id.toString() === prodId.toString()
+    );
+    if (alreadyadded) {
+      let user = await User.findByIdAndUpdate(
+        _id,
+        {
+          $pull: { wishlist: prodId },
+        },
+        { new: true }
+      );
+      res.json(user);
+    } else {
+      let user = await User.findByIdAndUpdate(
+        _id,
+        {
+          $push: { wishlist: prodId },
+        },
+        { new: true }
+      );
+      res.json(user);
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const rating = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, prodId, comment } = req.body;
+  try {
+    const product = await Product.findById(prodId);
+    const alreadyRated = await product.ratings.find(
+      (userId) => userId.postedby.toString() === _id.toString()
+    );
+    console.log(alreadyRated);
+    if (alreadyRated) {
+      const updateRating = await Product.updateOne(
+        {
+          ratings: { $elemMatch: alreadyRated },
+        },
+        {
+          $set: { "ratings.$.star": star, "ratings.$.comment": comment },
+        },
+        {
+          new: true,
+        }
+      );
+    } else {
+      const rateProduct = await Product.findByIdAndUpdate(prodId, {
+        $push: {
+          ratings: {
+            star: star,
+            comment: comment,
+            postedby: _id,
+          },
+        },
+      });
+    }
+    const getallratings = await Product.findById(prodId);
+    let totalRating = getallratings.ratings.length;
+    let ratingsum = getallratings.ratings
+      .map((item) => item.star)
+      .reduce((prev, curr) => prev + curr, 0);
+    let actualrating = Math.round(ratingsum / totalRating);
+    const finalProduct = await Product.findByIdAndUpdate(
+      prodId,
+      {
+        totalrating: actualrating,
+      },
+      { new: true }
+    );
+    res.json(finalProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
 module.exports = {
   createProduct,
   getaProduct,
@@ -126,4 +211,6 @@ module.exports = {
   updateProduct,
   filterProduct,
   deleteProduct,
+  addToWishlist,
+  rating,
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,6 +5,8 @@ const {
   getAllProduct,
   updateProduct,
   deleteProduct,
+  addToWishlist,
+  rating,
 } = require("../controller/productControllar");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -12,6 +14,8 @@ const router = express.Router();
 router.post("/", authMiddleware, isAdmin, createProduct);
 router.get("/", getAllProduct);
 router.get("/:id", getaProduct);
+router.put("/wishlist", authMiddleware, addToWishlist);
+router.put("/rating", authMiddleware, rating);
 router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 
