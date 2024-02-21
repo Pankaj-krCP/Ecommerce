3c8fb3843c16b03290e428f2be88d2e2f46f2c0a
pankaj <pankajkumar70792@gmail.com>
2024-02-21 15:13:01 +0530

    Added orders, wishlist, apply coupon, cart API

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -14,6 +14,17 @@ const {
   updatePassword,
   forgotPasswordToken,
   resetPassword,
+  createOrder,
+  getOrders,
+  updateOrderStatus,
+  getAllOrders,
+  getOrderByUserId,
+  getWishlist,
+  saveAddress,
+  userCart,
+  getUserCart,
+  emptyCart,
+  applyCoupon,
 } = require("../controller/userControllar.js");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware.js");
 
@@ -27,10 +38,27 @@ router.post("/admin-login", loginAdmin);
 router.get("/all-users", getallUser);
 router.get("/refreshtoken", handleRefreshToken);
 router.get("/logout", logOut);
-router.get("/:id", authMiddleware, isAdmin, getaUser);
-router.delete("/:id", deleteaUser);
+router.get("/wishlist", authMiddleware, getWishlist);
+router.post("/cart", authMiddleware, userCart);
+router.get("/cart", authMiddleware, getUserCart);
+router.delete("/empty-cart", authMiddleware, emptyCart);
+router.post("/cart/applycoupon", authMiddleware, applyCoupon);
+router.post("/cart/cash-order", authMiddleware, createOrder);
+router.get("/get-orders", authMiddleware, getOrders);
+router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
+router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getOrderByUserId);
+router.put("/save-address", authMiddleware, saveAddress);
+
+router.put(
+  "/order/update-order/:id",
+  authMiddleware,
+  isAdmin,
+  updateOrderStatus
+);
 router.put("/edit-user", authMiddleware, updatedUser);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unblock-user/:id", authMiddleware, isAdmin, unblockUser);
+router.get("/:id", authMiddleware, isAdmin, getaUser);
+router.delete("/:id", deleteaUser);
 
 module.exports = router;
