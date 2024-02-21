fba0ac2b59a41edcbaf5855f23ef6ba76d3b9722
pankaj <pankajkumar70792@gmail.com>
2024-02-21 12:21:55 +0530

    Added Color and Enquiry API and Model

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const categoryRouter = require("./routes/prodcategoryRoute");
 const blogcategoryRouter = require("./routes/blogcategoryRoute");
 const brandRouter = require("./routes/brandRoute");
 const couponRouter = require("./routes/couponRoute");
+const colorRouter = require("./routes/colorRoute");
+const enqRouter = require("./routes/enqRoute");
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
@@ -30,7 +32,8 @@ app.use("/api/category", categoryRouter);
 app.use("/api/blogcategory", blogcategoryRouter);
 app.use("/api/brand", brandRouter);
 app.use("/api/coupon", couponRouter);
-
+app.use("/api/color", colorRouter);
+app.use("/api/enquiry", enqRouter);
 app.use(notFound);
 app.use(errorHandler);
 
