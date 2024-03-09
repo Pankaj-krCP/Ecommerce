7b1347485591c13abd5c83d740381a12b2a377fc
pankaj <pankajkumar70792@gmail.com>
2024-03-09 17:28:41 +0530

    Added Filter feature to blog API

--- controller/blogControllar.js
+++ controller/blogControllar.js
@@ -46,8 +46,42 @@ const getBlog = asyncHandler(async (req, res) => {
 
 const getAllBlogs = asyncHandler(async (req, res) => {
   try {
-    const getAllBlogs = await Blog.find();
-    res.json(getAllBlogs);
+    // Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Blog.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const blogCount = await Blog.countDocuments();
+      if (skip >= blogCount) throw new Error("This Page does not exists");
+    }
+    const blog = await query;
+    res.json(blog);
   } catch (error) {
     throw new Error(error);
   }
